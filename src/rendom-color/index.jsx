afa59fe056f2f2722b07cb9d0056403170651e0c
go2023 <tetyanago2018@gmail.com>
2024-02-06 22:59:26 -0800

    RandomColor was adjusted

--- src/rendom-color/index.jsx
+++ src/rendom-color/index.jsx
@@ -1,12 +1,32 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function RandomColor() {
     const [typeOfColor, setTypeOfColor] = useState("hex");
     const [color, setColor] = useState("#000000");
 
-    const handleCreateRandomColor = () => {
-
+    const handleRandomColorUtility = (length) => {
+        return Math.floor(Math.random() * length);
     }
+    const handleCreateRandomHexColor = () => {
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+        let hexColor = "#";
+
+        for(let i=0; i<6; i++){
+            hexColor += hex[handleRandomColorUtility(hex.length)];
+        }
+        setColor(hexColor);
+    }
+
+    const handleCreateRandomRgbColor = () => {
+        const rgbColor = `rgb(${handleRandomColorUtility(255)}, ${handleRandomColorUtility(255)}, ${handleRandomColorUtility(255)})`;
+
+        setColor(rgbColor);
+    }
+    useEffect(() => {
+        if (typeOfColor === "rgb") handleCreateRandomRgbColor();
+        else handleCreateRandomHexColor();
+    }, [typeOfColor]);
+
     return (
         <div style={
             {
@@ -23,7 +43,24 @@ export default function RandomColor() {
             <h1>Random Color</h1>
             <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
             <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
-            <button onClick={handleCreateRandomColor}>Generate Random Color</button>
+            <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>
+                Generate Random Color
+            </button>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    color: "#fff",
+                    fontSize: "60px",
+                    marginTop: "50px",
+                    flexDirection: 'column',
+                    gap: '20px'
+                }}
+            >
+                <h3>{typeOfColor === "rgb" ? "RGB Color" : "HEX Color"}</h3>
+                <h1>{color}</h1>
+            </div>
         </div>
     )
 }
